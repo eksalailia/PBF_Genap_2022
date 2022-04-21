72db48405d67d0c35682901a00c7c61b2dab9f73
eksalailia <eksalailia@gmail.com>
2022-04-21 09:30:29 +0700

    manage global API service

--- Minggu_9/my-app/src/services/index.js
+++ Minggu_9/my-app/src/services/index.js
@@ -1,45 +1,9 @@
-const domainPath = 'http://localhost:3001';
-const GetAPI = (path) => {
-    const promise = new Promise((resolve, reject)=> {
-        fetch(`${domainPath}/${path}`)
-        .then(response => response.json())
-        .then((result)=>{
-            resolve(result);
-        }, (err) =>{
-            reject(err);
-        })
-    })
-    return promise;
-}
-const PostAPI = (path, data) => {
-    const promise = new Promise((resolve, reject)=> {
-        fetch(`${domainPath}/${path}`, {
-        method: 'post',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
+// const domainPath = 'http://localhost:3001';
+import GetAPI from "./Get";
+import PostAPI from "./Post";
+import DeleteAPI from "./Delete";
 
-        body: JSON.stringify(data)
-    })
-        .then((result)=>{
-            resolve(result);
-        }, (err) =>{
-            reject(err);
-        })
-    })
-    return promise;
-}
-const DeleteAPI = (path, data) =>{
-    const promise = new Promise((resolve, reject) => {
-        fetch(`${domainPath}/${path}/${data}`, {method: 'DELETE'})
-        .then((result)=> {
-            resolve(result);
-        }, (err) => {
-            reject(err);
-        })
-    })
-}
+
 const getNewsBlog = () => GetAPI('posts?_sort=id&_order=desc');
 const postNewsBlog = (dataYgDiKirim) => PostAPI('posts', dataYgDiKirim);
 const deleteNewsBlog = (dataYgDiHapus) => DeleteAPI('posts', dataYgDiHapus);
