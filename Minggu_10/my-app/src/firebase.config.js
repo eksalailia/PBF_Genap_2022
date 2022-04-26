7c9e0a64cd2468e56ce0c05f1a34ddd6e8a1c5b9
eksalailia <eksalailia@gmail.com>
2022-04-27 00:13:55 +0700

    Modul 10

---
+++ Minggu_10/my-app/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDwq5WLU1MmMnev8aglCXgmrVEm7B6ufKs",
+    authDomain: "minggu-10-firebase.firebaseapp.com",
+    projectId: "minggu-10-firebase",
+    storageBucket: "minggu-10-firebase.appspot.com",
+    messagingSenderId: "98946133794",
+    appId: "1:98946133794:web:99b039013e97473fa17f7b",
+    measurementId: "G-9EVC21YN0Q"
+  };
